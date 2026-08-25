02369914af10675bbe92267d98285b60847d178a
ajmat <ajmat@AIMEE_MATIAS>
2026-08-24 20:17:52 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Practica_1_Controles
+{
+    partial class Practica1Controles
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDia = new System.Windows.Forms.TextBox();
+            this.txtMes = new System.Windows.Forms.TextBox();
+            this.txtAnno = new System.Windows.Forms.TextBox();
+            this.lblDia = new System.Windows.Forms.Label();
+            this.lblMes = new System.Windows.Forms.Label();
+            this.lblAnno = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDia
+            // 
+            this.txtDia.Location = new System.Drawing.Point(128, 61);
+            this.txtDia.Name = "txtDia";
+            this.txtDia.Size = new System.Drawing.Size(100, 22);
+            this.txtDia.TabIndex = 0;
+            // 
+            // txtMes
+            // 
+            this.txtMes.Location = new System.Drawing.Point(128, 132);
+            this.txtMes.Name = "txtMes";
+            this.txtMes.Size = new System.Drawing.Size(100, 22);
+            this.txtMes.TabIndex = 1;
+            // 
+            // txtAnno
+            // 
+            this.txtAnno.Location = new System.Drawing.Point(128, 224);
+            this.txtAnno.Name = "txtAnno";
+            this.txtAnno.Size = new System.Drawing.Size(100, 22);
+            this.txtAnno.TabIndex = 2;
+            // 
+            // lblDia
+            // 
+            this.lblDia.AutoSize = true;
+            this.lblDia.Location = new System.Drawing.Point(69, 67);
+            this.lblDia.Name = "lblDia";
+            this.lblDia.Size = new System.Drawing.Size(28, 16);
+            this.lblDia.TabIndex = 3;
+            this.lblDia.Text = "Día";
+            // 
+            // lblMes
+            // 
+            this.lblMes.AutoSize = true;
+            this.lblMes.Location = new System.Drawing.Point(69, 138);
+            this.lblMes.Name = "lblMes";
+            this.lblMes.Size = new System.Drawing.Size(33, 16);
+            this.lblMes.TabIndex = 4;
+            this.lblMes.Text = "Mes";
+            // 
+            // lblAnno
+            // 
+            this.lblAnno.AutoSize = true;
+            this.lblAnno.Location = new System.Drawing.Point(69, 230);
+            this.lblAnno.Name = "lblAnno";
+            this.lblAnno.Size = new System.Drawing.Size(31, 16);
+            this.lblAnno.TabIndex = 5;
+            this.lblAnno.Text = "Año";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(425, 93);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(82, 16);
+            this.lblFecha.TabIndex = 6;
+            this.lblFecha.Text = "La fecha es: ";
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(243, 333);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 7;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(407, 333);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 8;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Practica1Controles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.lblAnno);
+            this.Controls.Add(this.lblMes);
+            this.Controls.Add(this.lblDia);
+            this.Controls.Add(this.txtAnno);
+            this.Controls.Add(this.txtMes);
+            this.Controls.Add(this.txtDia);
+            this.Name = "Practica1Controles";
+            this.Text = "Practica 1 Controles por Aimee Matias";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDia;
+        private System.Windows.Forms.TextBox txtMes;
+        private System.Windows.Forms.TextBox txtAnno;
+        private System.Windows.Forms.Label lblDia;
+        private System.Windows.Forms.Label lblMes;
+        private System.Windows.Forms.Label lblAnno;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_1_Controles
+{
+    public partial class Practica1Controles : Form
+    {
+        public Practica1Controles()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            if(txtDia.Text !="" && txtMes.Text !="" && txtAnno.Text!="" && txtDia.Text != " " && txtMes.Text != " " && txtAnno.Text != " ")
+            {
+                lblFecha.Text = "La fecha es: "+ txtDia.Text + "/" +txtMes.Text + "/" + txtAnno.Text;
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese los datos solicitados.");
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Se cerrará la ventana");
+            MessageBox.Show("Cerrando");
+            this.Close();
+        }
+    }
+}
